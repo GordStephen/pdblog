0b596b7140e00ab0d791307c418fc32487ae8e64
Gord Stephen <mail@gord.io>
2024-09-21 17:52:28 -0700

    Add Atom feed generation

--- pdblog.sh
+++ pdblog.sh
@@ -19,11 +19,57 @@ OUT_DIR="out"
 
 # === end config ===
 
-header="$THEME_DIR"/header.html
-post_template="$THEME_DIR"/post.html
-error_template="$THEME_DIR"/404.html
-footer="$THEME_DIR"/footer.html
+html_header_template="$THEME_DIR"/header.html
+html_footer_template="$THEME_DIR"/footer.html
+html_post_template="$THEME_DIR"/post.html
+html_error_template="$THEME_DIR"/404.html
+
+atom_header_template="$THEME_DIR"/header.xml
+atom_footer_template="$THEME_DIR"/footer.xml
+atom_entry_template="$THEME_DIR"/entry.xml
+
 highlight_file="$THEME_DIR"/highlighting.theme
+
+expand_template() {
+    pandoc --template="$1" \
+           --metadata title="$SITE_DESCRIPTION" \
+           --metadata author="$SITE_AUTHOR" \
+           --variable siteurl="$SITE_URL" \
+           --variable sitetitle="$SITE_TITLE" \
+           --variable sitesubtitle="$SITE_SUBTITLE" \
+           --variable sitefooter="$SITE_FOOTER" \
+           --variable now="$(date -Iseconds)" \
+           -t html < /dev/null
+}
+
+html_entry() {
+    humantime=$(date -d "$2" '+%B %-e, %Y')
+    printf '    <li>\n      <time datetime="%s">%s</time>\n      <a href="%s">%s</a>\n    </li>\n' "$2" "$humantime" "$3" "$1"
+}
+
+html_post() {
+    pandoc --template="$html_post_template" \
+           --metadata title="$1" \
+           --metadata author="$SITE_AUTHOR" \
+           --variable titlesuffix="$SITE_DESCRIPTION" \
+           --variable sitetitle="$SITE_TITLE" \
+           --variable sitesubtitle="$SITE_SUBTITLE" \
+           --variable humantime="$(date -d "$2" '+%B %-e, %Y')" \
+           --variable machinetime="$2" \
+           --variable sitefooter="$SITE_FOOTER" \
+           --highlight-style "$highlight_file" \
+           --shift-heading-level-by=1 \
+           -f "$POST_FORMAT" -t html "$3"
+}
+
+atom_entry() {
+    pandoc --template="$atom_entry_template" \
+           --metadata title="$1" \
+           --metadata author="$SITE_AUTHOR" \
+           --variable timestamp="$2" \
+           --variable url="$3" \
+           -f "$POST_FORMAT" -t html "$4"
+}
 
 if [ -d "$OUT_DIR" ]; then
     echo "Destination directory $OUT_DIR already exists, removing..."
@@ -35,15 +81,17 @@ fi
 mkdir -p "$OUT_DIR"
 homepage="$OUT_DIR"/index.html
 errorpage="$OUT_DIR"/404.html
+feed="$OUT_DIR"/atom.xml
+
+echo "Generating $errorpage..."
+expand_template "$html_error_template" > "$errorpage"
 
 echo "Generating $homepage..."
-echo "" | pandoc --template="$header" \
-                 --metadata title="$SITE_DESCRIPTION" \
-                 --metadata author="$SITE_AUTHOR" \
-                 --variable sitetitle="$SITE_TITLE" \
-                 --variable sitesubtitle="$SITE_SUBTITLE" \
-                 -t html  > "$homepage"
+expand_template "$html_header_template" > "$homepage"
 printf '\n<main>\n  <ol class="posts">\n' >> "$homepage"
+
+echo "Generating $feed..."
+expand_template "$atom_header_template" > "$feed"
 
 # [0-9]\{8\} doesn't seem to work, for some reason
 post_regex='[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]-*'$POST_EXTENSION
@@ -56,6 +104,7 @@ find $POSTS_DIR -name $post_regex | sort -r | while read file; do
     filename=$(basename "$file" $POST_EXTENSION)
     title=$(echo "$filename" | sed -E 's/^[0-9]{8}-(.*)$/\1/')
     timestamp=$(echo "$filename" | sed -E 's/^([0-9]{8})-.*$/\1/')
+    timestamp=$(date -d "$timestamp" '+%Y-%m-%d')
 
     # Process title text
     slug=$(echo "$title" | tr -dc '[:alnum:][:space:]'  | tr '[:upper:]' '[:lower:]' | tr -s ' -_'  | tr ' _' '-' )
@@ -65,47 +114,16 @@ find $POSTS_DIR -name $post_regex | sort -r | while read file; do
 
     echo "Processing $file to $page"
 
-    # Process timestamp
-    machinetime=$(date -d "$timestamp" '+%Y-%m-%d')
-    humantime=$(date -d "$timestamp" '+%B %-e, %Y')
-
-    # Create HTML page
     mkdir -p $(dirname "$page")
-    pandoc --template="$post_template" \
-           --metadata title="$title" \
-           --metadata author="$SITE_AUTHOR" \
-           --variable titlesuffix="$SITE_DESCRIPTION" \
-           --variable sitetitle="$SITE_TITLE" \
-           --variable sitesubtitle="$SITE_SUBTITLE" \
-           --variable humantime="$humantime" \
-           --variable machinetime="$machinetime" \
-           --variable sitefooter="$SITE_FOOTER" \
-           --highlight-style "$highlight_file" \
-           --shift-heading-level-by=1 \
-           -f $POST_FORMAT -t html "$file" > "$page"
-
-    # Update home page
-    printf '    <li>\n      <time datetime="%s">%s</time>\n      <a href="%s">%s</a>\n    </li>\n' "$machinetime" "$humantime" "$link" "$title" >> "$homepage"
+    html_post "$title" "$timestamp" "$file" > "$page"
+    html_entry "$title" "$timestamp" "$link" >> "$homepage"
+    atom_entry "$title" "$timestamp" "$url" "$file" >> "$feed"
 
 done
 
 printf '  </ol>\n</main>\n\n' >> "$homepage"
-echo "" | pandoc --template="$footer" \
-                 --metadata title="$SITE_DESCRIPTION" \
-                 --metadata author="$SITE_AUTHOR" \
-                 --variable sitetitle="$SITE_TITLE" \
-                 --variable sitesubtitle="$SITE_SUBTITLE" \
-                 --variable sitefooter="$SITE_FOOTER" \
-                 -t html >> "$homepage"
-
-echo "Generating $errorpage..."
-echo "" | pandoc --template="$error_template" \
-                 --metadata title="$SITE_DESCRIPTION" \
-                 --metadata author="$SITE_AUTHOR" \
-                 --variable sitetitle="$SITE_TITLE" \
-                 --variable sitesubtitle="$SITE_SUBTITLE" \
-                 --variable sitefooter="$SITE_FOOTER" \
-                 -t html  > "$errorpage"
+expand_template "$html_footer_template" >> "$homepage"
+expand_template "$atom_footer_template" >> "$feed"
 
 # Copy assets and theme includes
 
